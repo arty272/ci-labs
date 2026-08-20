05f71fc0fdac670f0167be00b5c5a4fbd784af45
arty2722 <arthur.ward-cox@computerpower.co.za>
2026-08-20 12:02:26 +0200

    Add Node.js app with CI workflow

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import { add, subtract } from './index';
-const { add, subtract } = require('./index');
+//const { add, subtract } = require('./index');
 let passed = 0, failed = 0;
 function assert(name, actual, expected) {
   if (actual === expected) { passed++; console.log('PASS: ' + name); }
